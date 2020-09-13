53685cbf896f81c2846202e93bac5971a9b96035
Bertuğ Fahri ÖZER <bertugfahriozer@gmail.com>
2020-09-13 14:28:04 +0300

    Views klasörüne ve yapısına giriş örnekleri oluşturuldu.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,14 +2,14 @@
 
 class Home extends BaseController
 {
-	public function index()
-	{
+    public function index()
+    {
         echo view('common/header');
         echo view('hello_world');
         echo view('common/footer');
-	}
+    }
 
-	//--------------------------------------------------------------------
+    //--------------------------------------------------------------------
     public function about()
     {
         echo view('common/header');
@@ -21,6 +21,22 @@ class Home extends BaseController
     {
         echo view('common/header');
         echo view('contact');
+        echo view('common/footer');
+    }
+
+    public function productList()
+    {
+        $data['products'] = [['productTitle' => 'NortPas Şınav Tahtası',
+                              'productCategory' => 'Spor Malzemeleri',
+                              'stock' => 34],
+                             ['productTitle' => 'HP Pavilion G135',
+                              'productCategory' => 'Laptop',
+                              'stock' => 3],
+                             ['productTitle' => 'Logitech Sessiz Fare',
+                              'productCategory' => 'Mouse',
+                              'stock' => 90]];
+        echo view('common/header');
+        echo view('products/productList', $data);
         echo view('common/footer');
     }
 }
---
+++ app/Views/products/productList.php
@@ -0,0 +1,49 @@
+<table class="table table-sm-responsive table-bordered table-striped">
+    <thead>
+    <td>Ürün Adı</td>
+    <td>Ürün Kategori</td>
+    <td>Ürün Adet</td>
+    </thead>
+    <tbody>
+    <?php foreach ($products as $product): ?>
+        <!--
+        ürün stoğu 10 dan azsa yeşil
+        ürün stoğu 5 ve 5 ten azsa turuncu
+        ürün stoğu 1 ise kırmızı
+        -->
+        <tr<?php
+
+       /* if ($product['stock'] < 10 && $product['stock']>5)
+            echo ' class="bg-success text-light font-weight-bold"';
+
+        elseif ($product['stock'] <= 5 && $product['stock']>1)
+            echo ' class="bg-warning text-light font-weight-bold"';
+
+        elseif ($product['stock'] == 1)
+            echo ' class="bg-danger text-light font-weight-bold"';
+
+        else
+            echo '';*/
+
+        switch ($product['stock']) {
+            case ($product['stock'] < 10 && $product['stock'] > 5):
+                echo ' class="bg-success text-light font-weight-bold"';
+                break;
+            case ($product['stock'] <= 5 && $product['stock'] > 1):
+                echo ' class="bg-warning text-light font-weight-bold"';
+                break;
+            case ($product['stock'] == 1):
+                echo ' class="bg-danger text-light font-weight-bold"';
+                break;
+            default:
+                echo '';
+                break;
+        }
+        ?>>
+            <td><?= $product['productTitle'] ?></td>
+            <td><?= $product['productCategory'] ?></td>
+            <td><?= $product['stock'] ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
